e073aadbaebba789e6995d43cfc0843a0a3a33ea
Lena Nordström <lena.nordstrom@medieinstitutet.se>
2019-07-17 17:40:17 +0200

    Updates

---
+++ precache-manifest.7e1f713ad66d9225f887ddbc95c6bab2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ed55578dbeef3d8049aa9031aeb208",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cb2b5e19d7d7b713dfeb",
+    "url": "/monsters-rolodex/static/css/main.15f8ec5d.chunk.css"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/monsters-rolodex/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "cb2b5e19d7d7b713dfeb",
+    "url": "/monsters-rolodex/static/js/main.aece32d9.chunk.js"
+  },
+  {
+    "revision": "49006584ddd3bfbd8db8",
+    "url": "/monsters-rolodex/static/js/runtime~main.26edc6c9.js"
+  }
+]);
